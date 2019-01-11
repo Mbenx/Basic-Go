79191bfead5d0896b3b8bcbe39dda7abf5175651
Mbenx <herlambang.srihartono@gmail.com>
2019-01-11 11:49:09 +0700

    use array in go

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Printf("hello, world\n")
+	// fix lenght array
+	// var arr [3]int
+	// arr[2] = 2
+	// fmt.Println(arr)
+
+	// // short hand array
+	// arr := [3]int{1,2,3}
+	// fmt.Println(arr)
+
+
+	// short hand dynamic array
+	arr := []int{1,2,3}
+	arr = append(arr, 4)
+	fmt.Println(arr)
+	
 }
