b752306302107e1d0b9b6a6f43c02de6fc406dc1
Mbenx <herlambang.srihartono@gmail.com>
2019-01-15 19:05:13 +0700

    Routing Hello World

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World")
+
+}
+
+func handleRequest() {
+
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", HelloWorld).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8081", router))
+}
 
 func main() {
-	fmt.Printf("hello, world\n")
+	fmt.Println("Go Run On Port 8081")
+
+	handleRequest()
 }
